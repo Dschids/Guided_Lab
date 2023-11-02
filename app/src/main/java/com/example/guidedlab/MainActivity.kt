df7905e499497a2b8296f35f61948a93d354b46c
Bob Dschida <rdschida@students.mchenry.edu>
2023-11-02 15:53:17 -0500

    Initial commit

---
+++ app/src/main/java/com/example/guidedlab/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.guidedlab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    enum class LoginSuccess
+        constructor(val intValue: Int){
+            login (1),
+            password (2),
+            success (0)
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val txtLogin = findViewById<EditText>(R.id.idEdtTxtUserName)
+        val txtPassword = findViewById<EditText>(R.id.idEdtTxtPassword)
+        val btnLogin = findViewById<Button>(R.id.idBtnLogin)
+
+        btnLogin.setOnClickListener {
+            when (CheckLogin(txtLogin.text.toString(), txtPassword.text.toString())) {
+                LoginSuccess.login -> {
+                    Toast.makeText(applicationContext, getString(R.string.errMsgLogin), Toast.LENGTH_LONG
+                    ).show()
+                    txtLogin.requestFocus()
+                }
+
+                LoginSuccess.password -> {
+                    Toast.makeText(applicationContext, getString(R.string.errMsgPassword), Toast.LENGTH_LONG).show()
+                    txtPassword.requestFocus()
+                }
+
+                else ->
+                    Toast.makeText(applicationContext, "That's the combination an idiot would put on his luggage", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    fun CheckLogin(txtLogin: String, txtPassword: String): LoginSuccess {
+        val holdLogin = "Bob"
+        val holdPass = "12345"
+
+        if (holdLogin != txtLogin) {
+            return LoginSuccess.login
+        }
+        return if (holdPass != txtPassword){
+            return LoginSuccess.password
+        } else LoginSuccess.success
+
+    }
+}
